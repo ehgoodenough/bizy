e5ab727acf4a538a7e0bfaaaee0751bc6cf893c0
Bill Brock <bbrock@Bills-MacBook-Pro.local>
2014-06-01 01:06:01 -0400

    terrible commit message 14

---
+++ getbizy_project/getbizy_project/social/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from crispy_forms.layout import Layout, Field, ButtonHolder, Submit, HTML, Div
+
+class SimpleProfileForm(forms.Form):
+
+
+    profile_photo = forms.ImageField(required=False)
+    description = forms.CharField(max_length=256, widget=forms.Textarea)
+    skills = forms.CharField(max_length=128, widget=forms.Textarea)
+    status = forms.CharField()
+
+
+class UserProfileForm(forms.Form):
+
+    first_name = forms.CharField(
+        max_length=80,
+        required=True,
+    )
+    last_name = forms.CharField(
+        max_length=80,
+        required=True,
+    )
+
+    email = forms.EmailField(required=True)
+    profile_photo = forms.ImageField(required=False)
+    description = forms.CharField(max_length=256, widget=forms.Textarea)
+    skills = forms.CharField(max_length=128, widget=forms.Textarea)
+    zip_code = forms.CharField(max_length=5)
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-exampleForm'
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Field('first_name', label='First Name'),
+            Field('last_name', label='Last Name'),
+            Field('email', label='Email Address'),
+            Field('profile_photo', label='Update Profile Picture'),
+            Field('description', label='Short Bio', rows="3"),
+            Field('skills', label='Your skills, comma delimeted', rows="3"),
+            Field('zip_code', label='Zip Code')
+        )
+
+        self.helper.add_input(Submit('submit', 'Submit'))
