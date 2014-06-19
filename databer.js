38dfba7e1102fff53841201f79eb9daf4b63a809
Andrew McPherson <andrewmcp333@gmail.com>
2014-06-19 17:03:01 -0400

    Imported the styles from the signup splashpage.

--- databer.js
+++ databer.js
@@ -1,14 +1,9 @@
-var placeholders = [
-	{
-		name: "Lenardo Da Vinci",
-		picture: "/images/pics/Leonardo_Da-Vinci-img1.jpg"
-	}
-];
-
 module.exports = function(mongo)
 {
 	mongo.users.drop();
 
+	var placeholders = require("./databer.placeholders.js");
+	
 	for(var index in placeholders)
 	{
 		mongo.users.insert(placeholders[index]);
---
+++ databer.placeholders.js
@@ -0,0 +1,6 @@
+module.exports = [
+	{
+		name: "Lenardo Da Vinci",
+		picture: "/images/davinci.jpg"
+	}
+];
